a4c2d8f8b63638652640fd2a2388de50eb9c3273
FangtaiDong10 <fangtaidong10@gmail.com>
2022-08-02 14:46:18 +1000

    new frontend

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,24 @@
 <script setup>
-import StudentsView from './components/StudentsView.vue';
-import { NConfigProvider, darkTheme } from 'naive-ui';
+import { RouterLink, RouterView } from "vue-router";
+import {
+  NConfigProvider,
+  NGlobalStyle,
+  darkTheme,
+  NMessageProvider,
+} from "naive-ui";
+import MessageContent from "./components/MessageContent.vue";
 </script>
 
 <template>
-  <NConfigProvider :theme="darkTheme">
-    <StudentsView msg="TODO LIST" />
-  </NConfigProvider>
+  <n-config-provider :theme="darkTheme">
+    <n-message-provider>
+      <message-content />
+      <n-global-style />
+      <RouterView />
+    </n-message-provider>
+  </n-config-provider>
 </template>
 
 <style>
-@import "./assets/base.css";
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-}
+@import "@/assets/base.css";
 </style>
